aa5dc264dd7c700ad4dc88e23df4f83194236d6d
r7-1700 <hymanho@outlook.com>
2024-08-07 17:41:54 -0700

    Added AccountPage.css to restyle account home page

--- src/models/AccountDataModel.js
+++ src/models/AccountDataModel.js
@@ -7,17 +7,17 @@ Data strucuture for Account -- Account class
 */
 
 class Account {
-    constructor(name, email, username, balance = 0, currency = 'USD', preferences = {}, transactions = [], security = {}, settings = {}, backup = {}) {
+    constructor(name, email, username, balance = 0, currency = 'USD', preferences = {}, expenses = {}, security = {}, settings = {}, backup = {}) {
       this.name = name;
       this.email = email;
       this.username = username;
       this.balance = balance;
       this.currency = currency;
-      this.preferences = preferences;
-      this.transactions = transactions;
+      // this.preferences = preferences;
+      this.transactions = expenses;
       this.security = security;
       this.settings = settings;
-      this.backup = backup;
+     // could throw away -- this.backup = backup;
     }
   
     // Method to update balance
