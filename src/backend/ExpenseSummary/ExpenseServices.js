352ef25634b9e5c5288ed2df53e11c01483f81ec
r7-1700 <hymanho@outlook.com>
2024-08-07 18:03:38 -0700

    Created an Expenses class, and also the create expense form

---
+++ src/backend/ExpenseSummary/ExpenseServices.js
@@ -0,0 +1,44 @@
+// src/backend/ExpenseManagement.js
+
+import { firestore } from '../../firebase'; // Import your Firestore instance
+import Expense from '../../models/Expense'; // Import the Expense class
+
+// Function to add a new expense
+const addExpense = async (userEmail, expense) => {
+  try {
+    await firestore.collection('accounts').doc(userEmail).collection('expenses').add({
+      title: expense.title,
+      amount: expense.amount,
+      date: expense.date,
+      category: expense.category,
+      description: expense.description,
+    });
+    console.log('Expense added successfully');
+  } catch (error) {
+    console.error('Error adding expense:', error);
+  }
+};
+
+// Function to edit an existing expense
+const editExpense = async (userEmail, expenseId, updatedFields) => {
+  try {
+    const expenseRef = firestore.collection('accounts').doc(userEmail).collection('expenses').doc(expenseId);
+    await expenseRef.update(updatedFields);
+    console.log('Expense updated successfully');
+  } catch (error) {
+    console.error('Error updating expense:', error);
+  }
+};
+
+// Function to delete an expense
+const deleteExpense = async (userEmail, expenseId) => {
+  try {
+    const expenseRef = firestore.collection('accounts').doc(userEmail).collection('expenses').doc(expenseId);
+    await expenseRef.delete();
+    console.log('Expense deleted successfully');
+  } catch (error) {
+    console.error('Error deleting expense:', error);
+  }
+};
+
+export { addExpense, editExpense, deleteExpense };
--- src/backend/auth/signUp.js
+++ src/backend/auth/signUp.js
@@ -1,4 +1,4 @@
-import { auth } from '../firebase';
+import { auth } from '../../firebase';
 import { createUserWithEmailAndPassword} from 'firebase/auth';
 
 export const signUp = async (email, password) => {
