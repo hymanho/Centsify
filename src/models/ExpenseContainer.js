47a886be6b69d194c6cd83bde70311118de9f2f3
r7-1700 <hymanho@outlook.com>
2024-08-12 22:21:08 -0700

    nothing happened

--- src/models/ExpenseContainer.js
+++ src/models/ExpenseContainer.js
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { getExpenses, addExpense, editExpense, deleteExpense } from '../backend/Account/ExpenseManagement/ExpenseService'; // Adjust the path as necessary
 
+
+
 const ExpenseContainer = ({ userEmail }) => {
   const [expenses, setExpenses] = useState([]);
 
