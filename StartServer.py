d19084696c16a19f8c68c4db049ecfd126e41de4
r7-1700 <hymanho@outlook.com>
2024-09-01 21:45:39 -0700

    Updated package.json to auto-start chatbot server

--- StartServer.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Replace 'path/to/your/server.py' with the actual path to your server script
-server_script = './StartServer.py'
-
-# Start the server
-os.system(f'python {server_script}')
