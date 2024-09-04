9c92ca7a9535e6e810dd8381fc3295642c5de2b6
r7-1700 <hymanho@outlook.com>
2024-09-04 16:13:56 -0700

    Re-updated main.yml file

--- src/backend/Chatbot/test.py
+++ src/backend/Chatbot/test.py
@@ -1,13 +1,7 @@
 import os
 def test_env_variable():
     firebase_admin_key = os.getenv('FIREBASE_ADMIN_KEY')
-    
-    if firebase_admin_key:
-        print("Successfully pulled FIREBASE_ADMIN_KEY from environment variables.")
-        # Optional: For debugging purposes, print the first few characters of the secret (avoid printing the full secret for security reasons)
-        print(f"First 50 characters of FIREBASE_ADMIN_KEY: {firebase_admin_key[:50]}")
-    else:
-        print("FIREBASE_ADMIN_KEY environment variable is not set.")
+    print(firebase_admin_key)
 
 # Run the test
 test_env_variable()
